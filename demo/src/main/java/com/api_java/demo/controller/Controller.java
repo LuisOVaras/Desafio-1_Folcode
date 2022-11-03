28d919140916065d461cb8790c306e8c31319d59
LuisOVaras <vluis2386@gmail.com>
2022-11-03 16:49:53 -0300

    terminando el desafio

--- demo/src/main/java/com/api_java/demo/controller/Controller.java
+++ demo/src/main/java/com/api_java/demo/controller/Controller.java
@@ -4,19 +4,16 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
-
     @GetMapping("/hello")
     public String hola() {
 
         return "Hola mi nombre es Luis Varas y esta es mi primera API";
 
     }
-
     @PostMapping("/goodbye")
     public String chau() {
 
         return "Hasta luego";
 
     }
-
 }
