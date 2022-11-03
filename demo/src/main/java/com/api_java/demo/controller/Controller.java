b366e71d89f6b6f444ddec628fd0261615e76aac
LuisOVaras <vluis2386@gmail.com>
2022-11-03 16:24:13 -0300

    Primer commit desafio 1

---
+++ demo/src/main/java/com/api_java/demo/controller/Controller.java
@@ -0,0 +1,22 @@
+package com.api_java.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hola() {
+
+        return "Hola mi nombre es Luis Varas y esta es mi primera API";
+
+    }
+
+    @PostMapping("/goodbye")
+    public String chau() {
+
+        return "Hasta luego";
+
+    }
+
+}
